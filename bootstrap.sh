2dbd21e08af243de06639e8f06ee825a073b0916
Achim Gsell <achim.gsell@psi.ch>
2016-10-12 15:20:54 +0200

    bootstrap.sh - exit if something went wrong

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 declare -r BASE_DIR=$(cd "$(dirname $0)" && pwd)
 
-${BASE_DIR}/compile_pmodules.sh
-${BASE_DIR}/install_pmodules.sh
+${BASE_DIR}/compile_pmodules.sh || exit 1
+${BASE_DIR}/install_pmodules.sh || exit 1
 
