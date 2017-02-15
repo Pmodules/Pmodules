8f1fa90b1a71cc4b51cc6c43ee57301d31ab89f8
Achim Gsell <achim.gsell@psi.ch>
2017-02-15 16:53:24 +0100

    build of 'dialog' removed

--- compile_pmodules.sh
+++ compile_pmodules.sh
@@ -62,10 +62,6 @@ if [[ ! -f "${PMODULES_HOME}/bin/getopt" ]] || [[ ${force} == 'yes' ]]; then
 	build getopt "${GETOPT_VERSION}"
 fi
 
-#if [[ ! -f "${PMODULES_HOME}/sbin/dialog" ]] || [[ ${force} == 'yes' ]]; then
-#	build dialog "${DIALOG_VERSION}"
-#fi
-
 if [[ ! -f "${PMODULES_HOME}/sbin/bash" ]] || [[ ${force} == 'yes' ]]; then
 	build bash "4.3.30"
 fi
