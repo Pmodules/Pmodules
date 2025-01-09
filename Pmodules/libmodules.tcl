fd1cd1c2e4edfb11b50e5af0579638a96b097c38
Achim Gsell <achim.gsell@psi.ch>
2025-01-09 10:24:12 +0100

    modulecmd: init global variable in libmodules.tcl

--- Pmodules/libmodules.tcl
+++ Pmodules/libmodules.tcl
@@ -27,6 +27,12 @@ package require base64
 set ::MODULEFILES_DIR "modulefiles"
 set ::ol_replacing "r"
 
+# variable derived from PMODULES_ENV
+set ::Dir2OverlayMap {}
+set ::OverlayInfo {}
+set ::UsedGroups {}
+set ::UsedOverlays {}
+
 proc _pmodules_parse_pmodules_env { } {
 	#
 	# In this library we need the value of some BASH variables
